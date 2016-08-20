5881242da3df5e716580ecae362602392651b5a8
Albert <alniss@gmail.com>
2016-08-20 20:03:00 -0300

    Start of the weather tab

---
+++ WL/WL.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+using namespace std;
+
+int GetHumidity(void)
+{
+	for (int i=0;i<20;i++)
+	{
+		int fp=open("/sys/bus/iio/devices/iio:device0/in_humidityrelative_input",O_RDONLY ); 
+		char buf[50];
+		int count=read(fp,buf,50);
+		close(fp);
+
+		if (count > 0)
+			return atoi(buf);
+	}
+	return 0;
+}
+
+int GetTemp(void)
+{
+	for (int i=0;i<20;i++)
+	{
+		int fp=open("/sys/bus/iio/devices/iio:device0/in_temp_input",O_RDONLY ); 
+		char buf[50];
+		int count=read(fp,buf,50);
+		close(fp);
+
+		if (count > 0)
+			return atoi(buf);
+	}
+	return 0;
+}
+
+int main(void)
+{
+	time_t t=time(0);
+	struct tm *now = localtime(&t);
+	
+	cout << setw(2) << setfill('0') << now->tm_mday << "/";
+	cout << setw(2) << setfill('0') << now->tm_mon+1 << "/";
+	cout << now->tm_year+1900 << " ";
+	cout << setw(2) << setfill('0') << now->tm_hour << ":";
+	cout << setw(2) << setfill('0') << now->tm_min  << ";";
+	cout << fixed << setprecision(1) << GetHumidity()/1000.0 << ";";
+	cout << fixed << setprecision(1) << GetTemp()/1000.0 << "\n";
+
+	return 0;
+}
